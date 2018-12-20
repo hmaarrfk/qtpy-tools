566e6eee3781d6b9e4218bfb4e1a6e56cbb2e007
Mark Harfouche <mark.harfouche@gmail.com>
2018-12-20 08:39:31 -0500

    Add versioneer to the setup.py

--- setup.py
+++ setup.py
@@ -6,17 +6,9 @@ from os import path
 with open('README.md') as f:
     long_description = f.read()
 
-# Loads version.py module without importing the whole package.
-def get_version_and_cmdclass(package_path):
-    import os
-    from importlib.util import module_from_spec, spec_from_file_location
-    spec = spec_from_file_location('version',
-                                   os.path.join(package_path, '_version.py'))
-    module = module_from_spec(spec)
-    spec.loader.exec_module(module)
-    return module.__version__, module.cmdclass
-
-version, cmdclass = get_version_and_cmdclass('qtpyuic')
+import versioneer
+version = versioneer.get_version()
+cmdclass = versioneer.get_cmdclass()
 
 setup(
     name='qtpy-uic',
